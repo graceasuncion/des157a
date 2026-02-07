a8127a2c03f6952ae2a6ea772d8a8956bcc40ba5
graceasuncion <ghasuncion@ucdavis.edu>
2026-02-06 19:44:26 -0800

    html + css basically done

---
+++ madlibs/v1/script.js
@@ -0,0 +1,8 @@
+(function(){
+    
+    const form = document.getElementById("madlibForm");
+    const storyDiv = document.getElementById("story");
+    const madlibText = document.getElementById("madlibText");
+
+
+}());
---
+++ madlibs/v2-final/script.js
@@ -0,0 +1,13 @@
+(function(){
+    
+    const form = document.getElementById("madlibForm");
+    const storyDiv = document.getElementById("story");
+    const madlibText = document.getElementById("madlibText");
+
+    form.addEventListener("submt", function(event){
+        
+
+
+    });
+
+}());
