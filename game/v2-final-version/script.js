86b639651b87c643cc0f631320e16932369ccd1d
graceasuncion <ghasuncion@ucdavis.edu>
2026-03-12 02:18:48 -0700

    start the code to swtich screens to start the game

--- game/v2-final-version/script.js
+++ game/v2-final-version/script.js
@@ -1,5 +1,18 @@
 (function(){
 
+    "use script";
 
+    console.document("reading js");
+
+    const startBtn = document.querySelector('#start-btn');
+    const startScreen = document.querySelector('#startscreen');
+    const gameScreen = document.querySelector('#gamescreen');
+
+    startBtn.addEventListener("click", function(){
+
+       startScreen.style.display ="none";
+       gameScreen.style.display = "block";
+
+    });
 
 }());
