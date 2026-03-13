7c8d04809102942c36bd34a9088cfe15ef93f4d6
graceasuncion <ghasuncion@ucdavis.edu>
2026-03-13 01:33:59 -0700

    making the coin flip

--- game/v2-final-version/script.js
+++ game/v2-final-version/script.js
@@ -4,6 +4,8 @@
 
     console.log("reading js");
 
+    /*the switch from the starting screen to the game screen*/
+
     const startBtn = document.querySelector('#start-btn');
     const startScreen = document.querySelector('#startscreen');
     const gameScreen = document.querySelector('#gamescreen');
